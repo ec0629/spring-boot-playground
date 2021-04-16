757d0c5052d16c1aae9dc30c732dd333cffc626f
Jeff Simonitto <jsimonitto@gmail.com>
2021-04-16 09:43:21 -0400

    Added controller endpoints and ModelAttributes. Basic page skeleton without Thymeleaf logic.

--- src/main/java/com/jeffsimonitto/spring/springbootplayground/controllers/Home.java
+++ src/main/java/com/jeffsimonitto/spring/springbootplayground/controllers/Home.java
@@ -1,15 +1,71 @@
 package com.jeffsimonitto.spring.springbootplayground.controllers;
 
+import com.jeffsimonitto.spring.springbootplayground.entities.*;
+import com.jeffsimonitto.spring.springbootplayground.services.SeedStarterService;
+import com.jeffsimonitto.spring.springbootplayground.services.VarietyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class Home {
 
+    @Autowired
+    private VarietyService varietyService;
+
+    @Autowired
+    private SeedStarterService seedStarterService;
+
+    @ModelAttribute("allTypes")
+    public List<Type> populateTypes() {
+        return Arrays.asList(Type.ALL);
+    }
+
+    @ModelAttribute("allFeatures")
+    public List<Feature> populateFeatures() {
+        return Arrays.asList(Feature.ALL);
+    }
+
+    @ModelAttribute("allVarieties")
+    public List<Variety> populateVarieties() {
+        return this.varietyService.findAll();
+    }
+
+    @ModelAttribute("allSeedStarters")
+    public List<SeedStarter> populateSeedStarters() {
+        return this.seedStarterService.findAll();
+    }
+
     @GetMapping
     public String index() {
+        return "index";
+    }
+
+    @PostMapping(params = {"save"})
+    public String saveSeedStarter(final SeedStarter seedStarter, final BindingResult bindingResult, final ModelMap model) {
+        if (bindingResult.hasErrors()) {
+            return "index";
+        }
+        this.seedStarterService.add(seedStarter);
+        model.clear();
+        return "redirect:/";
+    }
+
+    @PostMapping(params = {"addRow"})
+    public String addRow(final SeedStarter seedStarter) {
+        seedStarter.getRows().add(new Row());
+        return "index";
+    }
+
+    @PostMapping
+    public String removeRow(@RequestParam("removeRow") final int rowId, final SeedStarter seedStarter) {
+        seedStarter.getRows().remove(rowId);
         return "index";
     }
 }
